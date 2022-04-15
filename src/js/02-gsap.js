8131bb827b3f70093fa769f331c01dabf7708ed9
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-04-15 14:53:17 -0500

    15/05

--- src/js/02-gsap.js
+++ src/js/02-gsap.js
@@ -1,19 +1,19 @@
 gsap.registerPlugin(ScrollTrigger); //llamamos al plugin
 
-// trigger - le da scroll al objeto que le pongamos al parentesis
-// markers - nos indica las marcas donde el scroll se activa
 // star: el primer top hace referencia al top de nuestro elemento y el segundo al top de la pantalla con end aplica el mismo principio
+
 // scrub hace que la animación retroceda con el scroll en reversa, por defeto tien el parametro 'true' pero podemos agregar un delay para alentar la animación, en este ejemplo 4
+
 // pin hace que la animación siempre se vea en la pantalla
 
 /* var tl = gsap.timeline({
     scrollTrigger:{
-        trigger: '.ejem_1',
-        markers: 'true',
+        trigger: '.ejem_1',// elegimos a que elemento se va animar
+        markers: 'true', // activamos las marcas 
         start: 'top top',
         end : '+=900 0',
         scrub: '4',
-        pin: '.ejem_2',
+        pin: '.ejem_2', // podemos poner true para clavar todos los elementos del contenedor o poner sólo la que queremos clavar como en este ejemplo
     }
 });
 
@@ -26,11 +26,11 @@ var tl = gsap.timeline({
         trigger: 'contenedor__img', // el contenedor debe ir aquí 
         markers: 'true',
         start: 'top top',
-        end : '+=200 0',
-        scrub: '1',
+        end : '+=400 0',
+        scrub: '2',
         pin: 'true',
     }
 });
 
-tl.to('.ejem_1',{ x: '1000', duration: 2}, '-=9');
-tl.to('.ejem_2',{ x: '1000', duration: 2});
+tl.to('.ejem_1',{ x: '1000', duration: 2});
+tl.to('.ejem_2',{ x: '1000', duration: 2}, '-=.2');
